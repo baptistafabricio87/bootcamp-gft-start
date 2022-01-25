e8a7b407b6eb7e48ad5c53bfc6dc75a2643a2d6f
Fabricio Baptista de Castro <baptistafabricio87@gmail.com>
2022-01-25 16:51:43 -0300

    Exercicio - Interface - Calculadora

---
+++ orientacaoObjetos/estruturaBasica/interface/src/Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora implements OperacaoMatematica{
+
+    @Override
+    public void soma(double operando1, double operando2) {
+        System.out.println("SOMA: " + (operando1 + operando2));
+    }
+
+    @Override
+    public void subtracao(double operando1, double operando2) {
+        System.out.println("SUBTRACAO: " + (operando1 - operando2));
+    }
+
+    @Override
+    public void divisao(double operando1, double operando2) {
+        System.out.println("DIVISAO: " + (operando1 / operando2));
+    }
+
+    @Override
+    public void multiplicacao(double operando1, double operando2) {
+        System.out.println("MULTIPLICACAO: " + (operando1 * operando2));
+    }
+}
---
+++ orientacaoObjetos/estruturaBasica/interface/src/Program.java
@@ -0,0 +1,12 @@
+public class Program {
+
+    public static void main(String[] args) {
+
+        Calculadora calc = new Calculadora();
+
+        calc.soma(2, 4);
+        calc.subtracao(2, 4);
+        calc.divisao(4, 2);
+        calc.multiplicacao(2, 4);
+    }
+}
