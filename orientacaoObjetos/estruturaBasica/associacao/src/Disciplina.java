cafcc0fe2325cbf8c1e237d5bf01a3459ee80680
Fabricio Baptista de Castro <baptistafabricio87@gmail.com>
2022-01-25 16:29:59 -0300

    Exercicio - Associacao/Agregacao/Composicao

---
+++ orientacaoObjetos/estruturaBasica/associacao/src/Disciplina.java
@@ -0,0 +1,6 @@
+public class Disciplina {
+
+    // Associacao do tipo estrutural - Agregacao
+    // SEM PARTE TODO
+    Aluno aluno;
+}
---
+++ orientacaoObjetos/estruturaBasica/associacao/src/Pessoa.java
@@ -0,0 +1,5 @@
+public class Pessoa {
+    // Associacao do tipo COMPOSICAO
+    // COM PARTE TODO
+    Endereco endereco;
+}
